66f128fea379c1818bd7029685dcc179cec837a3
saba jokhadze <saba.joxadze.04@gmail.com>
2024-02-17 22:59:09 +0400

    hangman game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpServiceService } from './http-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hangmanGame';
+  public word: string = '';
+  public hint: string = '';
+  public arr?: string[];
+  public arr1?: string[];
+  public livesLeft = 5;
+  public enteredCharacter = '';
+  public guessed = false;
+  public playerStatus = "not playing";
+  public guessedWords = 0;
+  constructor(private service: HttpServiceService) {}
+  ngOnInit(): void {}
+  playGame() {
+    this.service.getRandomWord().subscribe((word) => {
+      this.word = word.word;
+      this.hint = word.hint;
+      this.arr = this.word.split('');
+      this.arr1 = Array(this.arr.length).fill('_');
+      this.guessed = false
+      this.playerStatus = "playing"
+      console.log(word);
+      console.log(this.arr1);
+    });
+  }
+  validateInput() {
+    this.checkIfWin();
+
+    if (this.enteredCharacter !== '' && this.guessed !== true) {
+      if (!this.arr?.includes(this.enteredCharacter)) {
+        this.livesLeft -= 1;
+        if (this.livesLeft === 0) {
+          this.playerStatus = "game lost"
+        }
+      } else {
+        for (let index = 0; index < this.arr!.length; index++) {
+          if (this.arr![index] === this.enteredCharacter) {
+            this.arr1![index] = this.enteredCharacter;
+            this.checkIfWin();
+          }
+        }
+      }
+    }
+    this.enteredCharacter = '';
+  }
+  checkIfWin() {
+    const enteredWord = this.arr1?.join('');
+    if (enteredWord === this.word) {
+      this.guessed = true;
+      this.guessedWords += 1;
+      
+    } else if (this.enteredCharacter === this.word) {
+      for (let index = 0; index < this.arr1!.length; index++) {
+        this.arr1![index] = this.enteredCharacter.charAt(index);
+      }
+      this.guessed = true;
+      this.guessedWords += 1;
+      this.livesLeft += 1
+    } else {
+      this.guessed = false;
+    }
+  }
 }
---
+++ src/app/http-service.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Word } from './interface';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpServiceService {
+  constructor(private http: HttpClient) {}
+
+  public getRandomWord() {
+    return this.http
+      .get<Word[]>(
+        'https://exercise-app-9b873-default-rtdb.europe-west1.firebasedatabase.app/words/-Nqndz_ggJvg8uAVG03F.json'
+      )
+      .pipe(
+        map((response: Word[]) => {
+          const randomIndex = Math.floor(Math.random() * response.length);
+          return response[randomIndex];
+        })
+      );
+  }
+}
